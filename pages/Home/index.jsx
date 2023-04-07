36a8e1785829a8bfe95ba535bc6791ca33ea3477
ujjwalbhattacharya07 <118751588+ujjwalbhattacharya07@users.noreply.github.com>
2023-04-07 14:32:15 +0530

    Update index.jsx

--- pages/Home/index.jsx
+++ pages/Home/index.jsx
@@ -5,15 +5,10 @@ import Footer from '../../components/Footer'
 import Creator from '../../components/Creator'
 import { useEffect, useState} from 'react'
 
-
-
-
 const home = () => {
-
   const [account, setAccount] = useState()
 
   useEffect(() => {
-
     if (window.ethereum) {
       // res[0] for fetching a first wallet
       window.ethereum
@@ -22,17 +17,21 @@ const home = () => {
     } else {
       alert('install metamask extension!!')
     }
-
-
   })
-  
+
+  const styles = {
+    container: {
+      background: 'linear-gradient(180deg, #01204c 0%, #7149e1 100%)',
+      minHeight: '100vh', // set a minimum height to ensure the gradient fills the whole page
+    },
+  };
+
   return (
-    <div className="bg-cyan-900">
-  
+    <div style={styles.container}>
       <Header />
       <Creator/>
       <Footer/>
-  </div>
+    </div>
   )
 }
 
