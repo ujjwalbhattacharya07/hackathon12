fed8f633f4c23af3d40b4ea83208936b64d145e0
ujjwalbhattacharya07 <118751588+ujjwalbhattacharya07@users.noreply.github.com>
2023-05-27 22:37:32 +0530

    Update index.jsx

--- pages/Dashboard/index.jsx
+++ pages/Dashboard/index.jsx
@@ -12,7 +12,7 @@ import Footer from '../../components/Footer'
 
 const dashboard = () => {
   const contractAddress = factoryAddress.tokenFactory
-  const web3 = new Web3('https://liberty20.shardeum.org')
+  const web3 = new Web3('https://rpc-testnet.5ire.network/')
 
   const [tokenAddresses, setTokenAddresses] = useState([])
   const [tokensCreated, setTokensCreated] = useState(0)
